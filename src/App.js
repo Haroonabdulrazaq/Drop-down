596b70bff2e6a4b7933d960adb6efb59b454fb75
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2021-07-14 17:58:35 +0100

    Add svg to component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { ReactComponent as BellIcon } from './icons/bell.svg'
+import { ReactComponent as PlusIcon } from './icons/plus.svg'
+import { ReactComponent as MessengerIcon } from './icons/messenger.svg'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <NavBar>
+     <NavItem icon={<BellIcon/>}/>
+     <NavItem icon={<PlusIcon/>}/>
+     <NavItem icon={<MessengerIcon/>}/>
+    </NavBar>
   );
+}
+
+function NavBar(props){
+  return (
+    <nav className="navbar"> 
+      <ul className="navbar-nav">
+        {props.children}
+      </ul>
+    </nav>
+  )
+}
+function NavItem(props){
+  return (
+    <li className="nav-item">
+      <a href="blog" className="icon-button">
+        {props.icon}
+      </a>
+    </li>
+  )
 }
 
 export default App;
