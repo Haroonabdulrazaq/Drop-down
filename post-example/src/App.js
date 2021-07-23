52f98d5a164779e4b40cba0a4f2f0dfb2968a19b
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2021-07-23 12:26:42 +0100

    Add redux to project

--- post-example/src/App.js
+++ post-example/src/App.js
@@ -1,9 +1,11 @@
 import PostComponent from './components/Post';
+import Form from './components/Form';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Form />
       <PostComponent />
     </div>
   );
--- post-example/src/components/Form.js
+++ post-example/src/components/Form.js
@@ -1,17 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import  './form.css'
 
 const Form = () => {
-  const handleSubmit =(e) => {
-    e.preventDefault()
-  }
-  const handleChange =() => {
+  const [post, setPost] = useState({
+    title: '',
+    body: ''
+  });
 
+  const handleSubmit =(e) => {
+    e.preventDefault();
+    console.log(post)
+    setPost({
+      title: '',
+      body: '',
+    })
+  }
+
+  const handleChange =(e) => {
+    const {name, value} = e.target;
+    setPost((prevPost) => ({
+      ...prevPost,
+      [name]: value,
+    }))
   }
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-        <input type="text" name="title"  onChange={handleChange}/>
-        <input type="text" name="body"  onChange={handleChange}/>
+      <h2>Create Post</h2>
+      <form onSubmit={handleSubmit} className="form">
+        <div className="form-control">
+          <input type="text" name="title" value={post.title} onChange={handleChange}/>
+        </div>
+        <div className="form-control">
+          <input type="text" name="body" value={post.body} onChange={handleChange}/>
+        </div>
         <button type="submit">Submit</button>
       </form>
     </div>
